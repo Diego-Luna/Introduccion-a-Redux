b59bef1f0a0af409f248d78539d9da6197705ba2
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-30 10:37:30 -0500

    Stateful vs Stateless

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Introduccion-a-Redux
+
+## Stateful vs Stateless
+Los componentes no funcionales no manejan estado, solo manejan información y funciones.
+
+Los componentes de clases manejan un estado interno, información y funciones, no hace falta definir las funciones con const simplemente con el nombre de la función ya lo detecta.
--- moonmakers-blog/src/components/App.js
+++ moonmakers-blog/src/components/App.js
@@ -1,43 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  let users = [
-    {
-      id: 1,
-      name: "Diego",
-      email: "diego@gmail.com",
-      domain: "moonmakers.com",
-    },
-    {
-      id: 1,
-      name: "Diego",
-      email: "diego@gmail.com",
-      domain: "moonmakers.com",
-    },
-  ];
+// const App = () => {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuarios: [
+        {
+          id: 1,
+          name: "Diego",
+          email: "diego@gmail.com",
+          web: "moonmakers.com",
+        },
+        {
+          id: 1,
+          name: "Diego Francisco",
+          email: "diegofco@gmail.com",
+          web: "moonmakers.com",
+        },
+      ],
+    };
+  }
 
-  return (
-    <div className="margen">
-      <table className="tabla">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Enlace</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((item) => (
-            <tr key={item.id}>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
-              <td>{item.domain}</td>
+  render() {
+    return (
+      <div className="margen">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+          </thead>
+          <tbody>
+            {this.state.usuarios.map((item) => (
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.web}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default App;
