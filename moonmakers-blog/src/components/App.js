ce0e54ceb766a7146cd45b3f5851222c79838ede
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-30 10:54:29 -0500

    Ciclo de vida de React

--- moonmakers-blog/src/components/App.js
+++ moonmakers-blog/src/components/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 // const App = () => {
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      usuarios: [],
+    };
+  }
+
+  async componentDidMount() {
+    const respuesta = await axios.get("https://jsonplaceholder.typicode.com/users");
+
+    console.log(respuesta);
+
+    this.setState({
       usuarios: [
         {
           id: 1,
@@ -13,16 +24,18 @@ class App extends Component {
           web: "moonmakers.com",
         },
         {
-          id: 1,
+          id: 2,
           name: "Diego Francisco",
           email: "diegofco@gmail.com",
           web: "moonmakers.com",
         },
       ],
-    };
+    });
   }
 
   render() {
+    console.log(this.state.usuarios);
+
     return (
       <div className="margen">
         <table className="tabla">
