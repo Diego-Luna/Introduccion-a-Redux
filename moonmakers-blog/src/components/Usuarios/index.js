3f9f44af8eea2e6508ccd9e18b02eae73a3f1999
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-31 09:33:21 -0500

    Usuarios, con arrow funcion

--- moonmakers-blog/src/components/Usuarios/index.js
+++ moonmakers-blog/src/components/Usuarios/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 
 // para conectar nuestro componente, a nuestro reducer
 import { connect } from "react-redux";
@@ -6,36 +6,48 @@ import { connect } from "react-redux";
 // importamos nuestros actions
 import * as usuariosAction from "../../actions/usuariosActions";
 
-class Usuarios extends Component {
-  componentDidMount() {
-    this.props.traerTodos();
-  }
+const Usuarios = (props) => {
+  const getUsuarios = async () => {
+    props.traerTodos();
+  };
 
-  render() {
-    return (
-      <div>
-        <table className="tabla">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Correo</th>
-              <th>Enlace</th>
+  useEffect(() => {
+    getUsuarios();
+  }, []);
+
+  const ponerFilas = () =>
+    props.usuarios.map((usuario) => (
+      <tr key={usuario.id}>
+        <td>{usuario.name}</td>
+        <td>{usuario.email}</td>
+        <td>{usuario.website}</td>
+      </tr>
+    ));
+
+  return (
+    <div>
+      <table className="tabla">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ponerFilas()}
+          {/* {this.props.usuarios.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.website}</td>
             </tr>
-          </thead>
-          <tbody>
-            {this.props.usuarios.map((item) => (
-              <tr key={item.id}>
-                <td>{item.name}</td>
-                <td>{item.email}</td>
-                <td>{item.website}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+          ))} */}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 // conectamos el estado a nuestro componente
 const mapStateToProps = (reducers) => {
@@ -43,3 +55,49 @@ const mapStateToProps = (reducers) => {
 };
 
 export default connect(mapStateToProps, usuariosAction)(Usuarios);
+
+// import React, { Component } from "react";
+
+// // para conectar nuestro componente, a nuestro reducer
+// import { connect } from "react-redux";
+
+// // importamos nuestros actions
+// import * as usuariosAction from "../../actions/usuariosActions";
+
+// class Usuarios extends Component {
+//   componentDidMount() {
+//     this.props.traerTodos();
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <table className="tabla">
+//           <thead>
+//             <tr>
+//               <th>Nombre</th>
+//               <th>Correo</th>
+//               <th>Enlace</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {this.props.usuarios.map((item) => (
+//               <tr key={item.id}>
+//                 <td>{item.name}</td>
+//                 <td>{item.email}</td>
+//                 <td>{item.website}</td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     );
+//   }
+// }
+
+// // conectamos el estado a nuestro componente
+// const mapStateToProps = (reducers) => {
+//   return reducers.usuarios;
+// };
+
+// export default connect(mapStateToProps, usuariosAction)(Usuarios);
