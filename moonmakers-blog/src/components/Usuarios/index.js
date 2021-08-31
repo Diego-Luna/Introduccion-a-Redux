e4f4e9d9a50ec814800f38ddd9f01c1d5b2a5c18
Dieg Luna <diegofcolunalopez@gmail.com>
2021-08-31 10:35:37 -0500

    Archivos Types

--- moonmakers-blog/src/components/Usuarios/index.js
+++ moonmakers-blog/src/components/Usuarios/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, { useEffect } from "react";
 
 // para conectar nuestro componente, a nuestro reducer
 import { connect } from "react-redux";
--- moonmakers-blog/src/reducers/usuarios.js
+++ moonmakers-blog/src/reducers/usuarios.js
@@ -1,11 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { TRAER_TODOS } from "../Types/usuauiosTypes"
+
+
 const INITIAL_STATE = {
   usuarios: [],
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "traer_usuarios":
+    case TRAER_TODOS:
       return {
         ...state,
         usuarios: action.payload,
