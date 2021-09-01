3ce70a3cc25cacf67c0be526d717f017ad697bb4
Dieg Luna <diegofcolunalopez@gmail.com>
2021-09-01 11:43:34 -0500

    Evitar sobrescritura

--- moonmakers-blog/src/components/Publicaciones/index.js
+++ moonmakers-blog/src/components/Publicaciones/index.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 
 import { connect } from "react-redux";
 
+import Cargando from "../General/Spinner";
+import Error from "../General/Error";
+
 // importamos nuestros actions
 import * as usuariosAction from "../../actions/usuariosActions";
 import * as publicacionesActions from "../../actions/publicacionesActions";
@@ -12,18 +15,54 @@ const { traerPorUsuario: publicacionesTraerPorUsuario } = publicacionesActions;
 // const Posts = (props) => {
 class Posts extends Component {
   async componentDidMount() {
+    const {
+      usuariosTraerTodos,
+      publicacionesTraerPorUsuario,
+      match: {
+        params: { key },
+      },
+    } = this.props;
+
+    // los estados no se pueden destructurar
     if (!this.props.usuariosReducer.usuarios.length) {
-      await this.props.usuariosTraerTodos();
+      await usuariosTraerTodos();
     }
-    this.props.publicacionesTraerPorUsuario(this.props.match.params.key);
+
+    if (this.props.usuariosReducer.error) {
+      return;
+    }
+    // si no tenemos publicaciones_key, lo llamamos
+    if (!("publicaciones_key" in this.props.usuariosReducer.usuarios[key])) {
+      publicacionesTraerPorUsuario(key);
+    }
   }
+
+  ponerUsuario = () => {
+    const {
+      usuariosReducer,
+      match: {
+        params: { key },
+      },
+    } = this.props;
+
+    if (usuariosReducer.error) {
+      return <Error mensaje={usuariosReducer.error} />;
+    }
+    if ( !usuariosReducer.usuarios.length|| usuariosReducer.cargando) {
+      return <Cargando />;
+    }
+
+    const nombre = usuariosReducer.usuarios[key].name;
+
+    return <h1>Publicaciones de {nombre}</h1>;
+  };
 
   render() {
     console.log(this.props);
     return (
       <div>
-        <h1>Publicaciones de</h1>
         {this.props.match.params.key}
+        {this.ponerUsuario()}
       </div>
     );
   }
