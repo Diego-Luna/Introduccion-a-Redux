e924966b0a0001a44d6e027d6b80b058b4e73bf0
Dieg Luna <diegofcolunalopez@gmail.com>
2021-09-03 09:39:21 -0500

    Mapear Objetos

--- moonmakers-blog/src/components/Tareas/index.js
+++ moonmakers-blog/src/components/Tareas/index.js
@@ -1,17 +1,56 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import Cargando from "../General/Spinner";
+import Error from "../General/Error";
+
 import * as tareasActions from "../../actions/tareasActions";
 
 class Tareas extends Component {
-
-  componentDidMount(){
-    this.props.traerTodas();
+  componentDidMount() {
+    if (!Object.keys(this.props.tareas).length) {
+      this.props.traerTodas();
+    }
   }
+
+  mostrarContenido = () => {
+    const { tareas, cargando, error } = this.props;
+
+    if (cargando) {
+      return <Cargando />;
+    }
+
+    if (error) {
+      return <Error mensaje={error} />;
+    }
+
+    // retornamos
+    return Object.keys(tareas).map((user_id) => (
+      <div key={user_id}>
+        <h2>Usuarios {user_id}</h2>
+        <div className="contenedor_tareas">{this.ponerTareas(user_id)}</div>
+      </div>
+    ));
+  };
+
+  ponerTareas = (user_id) => {
+    const { tareas } = this.props;
+    // solo pones las tareas solo por usuarios
+    const porUsuarios = {
+      ...tareas[user_id],
+    };
+
+    return Object.keys(porUsuarios).map((tar_id) => (
+      <div key={tar_id} >
+        <input type="checkbox" defaultChecked={porUsuarios[tar_id].completed} />
+        {porUsuarios[tar_id].title}
+      </div>
+    ));
+  };
 
   render() {
     console.log(this.props);
-    return <div>Tarea saludar</div>;
+    return <div>{this.mostrarContenido()}</div>;
   }
 }
 
